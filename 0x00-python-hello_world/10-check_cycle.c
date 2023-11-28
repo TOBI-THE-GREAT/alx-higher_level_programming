01521978cf496cc3eba933416ee23ee45ac81c44
TOBI-THE-GREAT <oluwatobiolukunle1@gmail.com>
2023-11-28 12:14:40 -0800

    10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
@@ -8,18 +9,21 @@
  */
 int check_cycle(listint_t *list)
 {
-        listint_t *slow = list;
-        listint_t *fast = list;
+        listint_t *slow, *fast;
+        
 
-        if (!list)
+        if (list == NULL || list->next == NULL)
                 return (0);
 
-        while (slow && fast && fast->next)
+        slow = list->next;
+	fast = list->next->next;
+	
+	while (slow && fast && fast->next)
         {
                 slow = slow->next;
                 fast = fast->next->next;
-                if (slow == fast)
-                        return (1);
+               
+                     
         }
 
         return (0);
